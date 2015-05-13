adf4708c74172aa0a97f3f860e0c62a90deca308
mskiba <cines91@gmail.com>
2015-05-13 14:31:44 +0200

    About me section

--- functions.php
+++ functions.php
@@ -88,6 +88,78 @@
                 )
             )
         );
+
+        // facebook link
+        $wp_customize->add_setting(
+            'facebook_field'
+        );
+
+        $wp_customize->add_control(
+            new WP_Customize_Control(
+                $wp_customize,
+                'facebook_field',
+                array(
+                    'label'          => 'Link to Facebook profile',
+                    'section'        => 'about_me_section',
+                    'settings'       => 'facebook_field',
+                    'type'           => 'text'
+                )
+            )
+        );
+
+        // github link
+        $wp_customize->add_setting(
+            'github_field'
+        );
+
+        $wp_customize->add_control(
+            new WP_Customize_Control(
+                $wp_customize,
+                'github_field',
+                array(
+                    'label'          => 'Link to Github profile',
+                    'section'        => 'about_me_section',
+                    'settings'       => 'github_field',
+                    'type'           => 'text'
+                )
+            )
+        );
+
+        // linke in link
+        $wp_customize->add_setting(
+            'linkedin_field'
+        );
+
+        $wp_customize->add_control(
+            new WP_Customize_Control(
+                $wp_customize,
+                'linkedin_field',
+                array(
+                    'label'          => 'Link to LinkedIn profile',
+                    'section'        => 'about_me_section',
+                    'settings'       => 'linkedin_field',
+                    'type'           => 'text'
+                )
+            )
+        );
+
+        // linke in link
+        $wp_customize->add_setting(
+            'email_field'
+        );
+
+        $wp_customize->add_control(
+            new WP_Customize_Control(
+                $wp_customize,
+                'email_field',
+                array(
+                    'label'          => 'Contact e-mail',
+                    'section'        => 'about_me_section',
+                    'settings'       => 'email_field',
+                    'type'           => 'text'
+                )
+            )
+        );
     }
     add_action( 'customize_register', 'about_me_customizer' );
 ?>
