287f24bb8b2c86efa890f913abf5caf3c94fce00
mskiba <cines91@gmail.com>
2015-05-15 13:05:17 +0200

    the_date fix

--- post/post_partials/meta-data.php
+++ post/post_partials/meta-data.php
@@ -1,6 +1,6 @@
 <div class="meta-data">
     <span class="pub-date">
-        <i class="fa fa-calendar"></i><?php the_date(); ?>
+        <i class="fa fa-calendar"></i><?php echo get_the_date(); ?>
     </span>
     <a href="<?php the_permalink() ?>#comments" class="comments">
         <i class="fa fa-comments"></i><?php comments_number( 'No responses', 'One response', '% responses' ); ?>
